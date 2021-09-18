de6a689eb1387ecc052c7601f535e27983163cda
XanderChua <72645612+XanderChua@users.noreply.github.com>
2021-09-18 13:19:32 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Stack
+{
+    public class Stack
+    {
+        static void Main(string[] args)
+        {
+            Stack<string> stack = new Stack<string>();//create the stack
+            stack.Push("A");
+            stack.Push("B");
+            stack.Push("C");
+            stack.Push("D");
+
+            foreach(string stackk in stack) //stack the string
+            {
+                Console.WriteLine(stackk); //display after stack
+            }
+
+            Console.WriteLine("Pop the first");
+            stack.Pop(); //pop 1 from the top
+            foreach (string pop in stack) 
+            {
+                Console.WriteLine(pop); //display after pop
+            }
+
+            Console.WriteLine("Peek");
+            Console.WriteLine(stack.Peek()); //display after peek
+
+        }
+    }
+}
